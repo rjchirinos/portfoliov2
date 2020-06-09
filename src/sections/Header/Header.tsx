e00fd577ad2dcb5dbf9d36b7921e61941d7f3cac
Raúl Chirinos <raulstuf@gmail.com>
2020-06-09 15:00:10 -0400

    Add Header Background

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,18 +1,26 @@
 import React, { Fragment } from 'react';
 import './Header.scss';
+import styled from 'styled-components';
 
 import Navbar from '../../components/Navbar/Navbar';
 import HeaderCont from '../../components/HeaderCont';
 import BgShapes from '../../assets/img/header-bg.svg';
 
+const Bg = styled.img`
+  width: 51.46%;
+  position: absolute;
+  right: 0;
+  transform: translateX(10%);
+`;
+
 export default function Header() {
   return (
-    <section style={{ backgroundImage: `url(${BgShapes})` }} className="header">
+    <section className="header">
       <Navbar />
       <div className="header_container">
         <HeaderCont />
       </div>
-      {/* <img src={BgShapes} /> */}
+      <Bg src={BgShapes} />
     </section>
   );
 }
